a91acd078be3ee1d129532807c3058f3b23c80a8
mahbubur1 <mdmahbuborr462@gail.com>
2022-04-07 12:43:24 +0600

    react bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import { getAuth } from "firebase/auth";
 import app from "./Firebase.init";
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./App.css";
 
-const auth = getAuth(app)
-const getEmailBlur = event =>{
+const auth = getAuth(app);
+const handleEmailBlur = (event) => {
   console.log(event.target.value);
-}
+};
+const handlePasswordBlur = (event) => {
+  console.log(event.target.value);
+};
+const handleSubmit = (event) => {
+  console.log("form submitted");
+  event.preventDefault();
+};
 function App() {
   return (
-    <div className="App">
-      <form>
-        <input onBlur={getEmailBlur} type="email" name="" id="" />
-        <input type="password" name="" id="" />
-      </form>
+    <div className="w-50 mx-auto mt-5"> 
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
 
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
